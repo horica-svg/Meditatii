fbb17073c3191c0a7291f786728baf31f5ac4aeb
horica-svg <horiazafiu2@gmail.com>
2025-06-24 19:53:36 +0300

    added the first two parts

---
+++ clasa a 9a/Introducere/Operatori/main.cpp
@@ -0,0 +1,42 @@
+//
+// Created by horia on 6/24/2025.
+//
+#include <iostream>
+
+using namespace std;
+
+int X;
+double Y;
+
+int main() {
+    X = 10;
+    Y = 30.27;
+    // operatorii sunt ceea ce ne ajuta sa realizam operatii intre variabile
+
+    // operatorii aritmetici unari (cu un singur operand)
+    cout<< -X <<endl;
+    cout<< +Y<<endl;
+    cout<< - + X<<endl;
+
+    // operatorii aritmetici binari (cu 2 operanzi, nu se refera la codul binar)
+    cout<<X - Y<<endl;
+    cout<<X + Y<<endl;
+    cout<<X * Y<<endl;
+    cout<<X / Y<<endl;
+    cout<<X % 3<<endl; // operatorul modulo - % - are sens doar daca ambii operanzi sunt intregi
+                       // si va returna restul impartirii lui X la 3
+
+    // operatorii relationali
+    // fie M si N
+    int M = 3;
+    int N = 11;
+    // operatorii relationali stabilesc valoarea de adevar a unei relatii dintre 2 operanzi
+    cout<<(M<N)<<endl;
+    cout<<(N<M)<<endl;
+    cout<<(N==M)<<endl;
+
+    cout<<(5 > 4 > 3)<<endl; // va returna fals!!
+    // relatia se evalueaza in urmatorul mod
+    // mai intai (5 < 4) ceea ce returneaza valoarea 1
+    // apoi se evalueaza (1 < 3) ceea ce este fals
+}
---
+++ clasa a 9a/Introducere/Tipuri de date/main.cpp
@@ -0,0 +1,35 @@
+//
+// Created by horia on 6/24/2025.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5; // numere naturale in intervalul [ -2^31, (2^31)-1 ]
+    float b = 2.5; // numere reale, memorate pe 4 octeti
+    double c = 3.5; // numere reale, memorate pe 8 octeti
+    char d = 'a'; // caractere (de exemplu litere ale alfabetului, semne de punctuatie si alte caractere speciale)
+    bool e = true; // boolean - retine doar true sau false, 1 sau 0
+    int* f = &a; // pointer - retine adresa din memorie a unei variabile
+    cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<endl;
+
+    // conersii de tip
+    // putem converti o variabila de un tip in altul
+
+    b = a; // b va fi 5
+    a = c; // a va fi 3, deoarece nu poate retine decat int
+
+    cout<<b<<" "<<a<<endl;
+
+    cout<<2 + 3.5<<endl; // va afisa 5.5, nu 5, pentru a nu avea pierderi de date
+
+    cout<<(int) d<<endl; // conversie explicita de la char la int
+    // va afisa codul ASCII al caracterului retinut in d
+    cout<<(char) 100<<endl; // va afisa 'd', caracterul corespunzator al codului 100 in ASCII
+
+    a = 30;
+    cout<<a/3<<endl; // va returna 10 (impartire a 2 intregi)
+    cout<<a/3.0<<endl; // va returna 10.0 (impartirea unui intreg la un float/double)
+}
